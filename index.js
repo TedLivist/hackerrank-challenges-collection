181454125a03491a8d84ca5289d6e6a7e4020865
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-05-25 17:53:47 +0100

    Added Pangrams solution

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+*/Pangram Challenge/*
 
+function pangram(s) {
+
+  let res = []
+  let i = 97
+  while (String.fromCharCode(i) <= String.fromCharCode(122)) {
+    res.push(String.fromCharCode(i))
+    i++
+  }
+
+  s = s.toLowerCase()
+
+  let my_check = true
+
+  for (let j in res) {
+    if (!s.includes(res[j])) {
+      my_check = false
+      break
+    }
+  }
+
+  if (my_check == false) {
+    return "not pangram"
+  } else {
+    return "pangram"
+  }
+}
